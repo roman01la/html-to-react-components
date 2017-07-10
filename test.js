594a9531809a932d4ed0e2d1d4e66f1fadfa2127
Roman Liutikov <roman01la@romanliutikov.com>
2017-07-10 20:33:21 +0300

    use Prettier & Jest

--- test.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var getComponentsLib = require('./lib');
-
-var html = fs.readFileSync('./test/basic.html', 'utf8');
-
-var components = getComponentsLib(html);
-
-console.log(components);
--- test/test.js
+++ test/test.js
@@ -1,111 +1,77 @@
 var fs = require('fs');
 var path = require('path');
-var assert = require('assert');
 var getComponentsLib = require('../lib');
 var html = readFile('basic.html');
-var expected = require('./expected');
 
 function readFile(file) {
   return fs.readFileSync(path.join(__dirname, file), 'utf8');
 }
 
-function assertKeysEqual(o1, o2) {
-  var o1k = Object.keys(o1);
-  var o2k = Object.keys(o2);
-
-  try {
-    assert.equal(o1k.length, o2k.length);
-
-    o1k.forEach(function(key) {
-      assert.ok(o2.hasOwnProperty(key));
-    });
-  } catch (err) {
-    err.expected = o1k;
-    err.actual = o2k;
-    err.showDiff = true;
-    throw err;
-  }
-
-}
-
-function equal(o1, o2) {
-
-  assertKeysEqual(o1, o2);
-
-  try {
-    Object.keys(o1).forEach(function(key) {
-      assert.equal(o1[key], o2[key]);
-    });
-  } catch (err) {
-    err.expected = o1;
-    err.actual = o2;
-    err.showDiff = true;
-    throw err;
-  }
-}
-
 describe('HTML to React components', function() {
-
   it('should generate ES5 React components', function() {
-
-    equal(expected[0], getComponentsLib(html, {
-      componentType: 'es5',
-      moduleType: false
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'es5',
+        moduleType: false
+      })
+    ).toMatchSnapshot();
   });
 
   it('should generate ES6 React components', function() {
-
-    equal(expected[1], getComponentsLib(html, {
-      componentType: 'es6',
-      moduleType: false
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'es6',
+        moduleType: false
+      })
+    ).toMatchSnapshot();
   });
 
   it('should generate stateless React components', function() {
-
-    equal(expected[2], getComponentsLib(html, {
-      componentType: 'stateless',
-      moduleType: false
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'stateless',
+        moduleType: false
+      })
+    ).toMatchSnapshot();
   });
 
   it('should generate ES5 React components as ES6 modules', function() {
-
-    equal(expected[3], getComponentsLib(html, {
-      componentType: 'es5',
-      moduleType: 'es6'
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'es5',
+        moduleType: 'es6'
+      })
+    ).toMatchSnapshot();
   });
 
   it('should generate ES5 React components as CommonJS modules', function() {
-
-    equal(expected[4], getComponentsLib(html, {
-      componentType: 'es5',
-      moduleType: 'cjs'
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'es5',
+        moduleType: 'cjs'
+      })
+    ).toMatchSnapshot();
   });
 
   it('should generate file names in lowercase with the specified delimiter', function() {
-
-    equal(expected[5], getComponentsLib(html, {
-      componentType: 'es5',
-      moduleType: 'es6',
-      moduleFileNameDelimiter: '-'
-    }));
+    expect(
+      getComponentsLib(html, {
+        componentType: 'es5',
+        moduleType: 'es6',
+        moduleFileNameDelimiter: '-'
+      })
+    ).toMatchSnapshot();
   });
 
-  it('should throw an error if there\'s data-component attribute without a value in HTML', function() {
-
-    try {
-      getComponentsLib(readFile('fail.html'));
-    } catch (error) {
-      assert.equal(error.message, 'There\'s annotated component without a name!');
-    }
+  it("should throw an error if there's data-component attribute without a value in HTML", function() {
+    expect(() => getComponentsLib(readFile('fail.html'))).toThrow();
   });
 
   it('should transform inline CSS properly', function() {
-
-    equal(expected[6], getComponentsLib('<div data-component="Box" style="margin:0;padding:10px;border:1px solid black;">Box</div>'));
+    expect(
+      getComponentsLib(
+        '<div data-component="Box" style="margin:0;padding:10px;border:1px solid black;">Box</div>'
+      )
+    ).toMatchSnapshot();
   });
 });
