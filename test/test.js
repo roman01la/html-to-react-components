e75c0549d11cc7592e8ffd8c903e7ec283e2d9f8
roman01la <roman01la@romanliutikov.com>
2019-01-26 13:15:35 +0200

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,74 +1,82 @@
-var fs = require("fs")
-var path = require("path")
-var getComponentsLib = require("../lib")
-var html = readFile("basic.html")
+var fs = require("fs");
+var path = require("path");
+var getComponentsLib = require("../lib");
+var html = readFile("basic.html");
 
 function readFile(file) {
-  return fs.readFileSync(path.join(__dirname, file), "utf8")
+  return fs.readFileSync(path.join(__dirname, file), "utf8");
 }
 
 describe("HTML to React components", function() {
   it("should generate ES5 React components", function() {
     expect(
       getComponentsLib(html, {
-        componentType: "es5",
-      }),
-    ).toMatchSnapshot()
-  })
+        componentType: "es5"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should generate ES6 React components", function() {
     expect(
       getComponentsLib(html, {
-        componentType: "es6",
-      }),
-    ).toMatchSnapshot()
-  })
+        componentType: "es6"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should generate stateless React components", function() {
     expect(
       getComponentsLib(html, {
-        componentType: "stateless",
-      }),
-    ).toMatchSnapshot()
-  })
+        componentType: "stateless"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should generate ES5 React components as ES6 modules", function() {
     expect(
       getComponentsLib(html, {
         componentType: "es5",
-        moduleType: "es6",
-      }),
-    ).toMatchSnapshot()
-  })
+        moduleType: "es6"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should generate ES5 React components as CommonJS modules", function() {
     expect(
       getComponentsLib(html, {
         componentType: "es5",
-        moduleType: "cjs",
-      }),
-    ).toMatchSnapshot()
-  })
+        moduleType: "cjs"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should generate file names in lowercase with the specified delimiter", function() {
     expect(
       getComponentsLib(html, {
         componentType: "es5",
         moduleType: "es6",
-        moduleFileNameDelimiter: "-",
-      }),
-    ).toMatchSnapshot()
-  })
+        moduleFileNameDelimiter: "-"
+      })
+    ).toMatchSnapshot();
+  });
 
   it("should throw an error if there's data-component attribute without a value in HTML", function() {
-    expect(() => getComponentsLib(readFile("fail.html"))).toThrow()
-  })
+    expect(() => getComponentsLib(readFile("fail.html"))).toThrow();
+  });
 
   it("should transform inline CSS properly", function() {
     expect(
       getComponentsLib(
-        '<div data-component="Box" style="margin:0;padding:10px;border:1px solid black;">Box</div>',
-      ),
-    ).toMatchSnapshot()
-  })
-})
+        '<div data-component="Box" style="margin:0;padding:10px;border:1px solid black;">Box</div>'
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("should transform public HTML attrbiutes into React props", function() {
+    expect(
+      getComponentsLib(
+        '<button data-component="Button" public:onclick="hello();">press</button>'
+      )
+    ).toMatchSnapshot();
+  });
+});
